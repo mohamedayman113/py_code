0929db537bee28250045e927f2106b4271e33479
mohamedayman113 <mohamedactoor229@gmail.com>
2021-10-13 14:34:24 +0200

    first commit

---
+++ if_stm.py
@@ -0,0 +1,15 @@
+# i=2044
+# if i % 2 == 1:
+#     print (f'{i} is numper')
+
+# elif i % 2 == 0 :
+        #  _____________________________________
+        # calendar@@@@@ 
+#     print (f'good luck')
+# import calendar
+# while True:
+#     year=int(input("year = "))
+#     month=int(input("month = "))
+#     print(calendar.month(year,month))
+# _________________________________________
+# login system
---
+++ list.py
@@ -0,0 +1,21 @@
+# l=[11,100,101,999,1001]
+# print(l[1])
+# answer_1=l[-1]
+# print (answer_1)
+# l=['socks','4k drone','wine','jam']
+# l.append('pajamas')
+# print (l)
+# l2=["socks","tshirt","pajamas"]
+# l.append(l2)
+# print(l)
+
+# lst=[55,6,777,54,6,76,101,1,6,2,6]
+# answer_1= max(lst)
+# answer_2= min(lst)
+# answer_3= lst.count(6)
+# summation = sum(lst)
+
+# print(answer_1)
+# print(answer_2)
+# print(answer_3)
+# print(summation)
